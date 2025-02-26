046bb558cbe600fd7d5c985e5e9111c767f89317
Hasyirin Fakhriy <ahrim45@gmail.com>
2025-02-26 11:47:12 +0800

    maybe remove scope alltogether

--- src/Concerns/InteractsWithCodeScope.php
+++ src/Concerns/InteractsWithCodeScope.php
@@ -4,17 +4,10 @@ declare(strict_types=1);
 
 namespace Hasyirin\Address\Concerns;
 
-use Illuminate\Database\Eloquent\Builder;
-
 trait InteractsWithCodeScope
 {
-    public function scopeOfCode(Builder $query, string $code): Builder
-    {
-        return $query->where('code', $code);
-    }
-
     public static function ofCode(string $code): ?static
     {
-        return static::query()->ofCode($code)->first();
+        return static::query()->firstWhere('code', $code);
     }
 }
