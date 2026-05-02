3283c4a29378e73aa5a8b006a4fb3471585b9edf
hasyirin <ahrim45@gmail.com>
2026-05-02 20:56:41 +0800

    update address factory to use building number and address

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -16,8 +16,8 @@ class AddressFactory extends Factory
     {
         return [
             'type' => fake()->randomElement(['primary', 'billing', 'shipping']),
-            'line_1' => fake()->streetAddress(),
-            'line_2' => fake()->optional()->secondaryAddress(),
+            'line_1' => fake()->buildingNumber(),
+            'line_2' => fake()->optional()->address(),
             'line_3' => null,
             'postcode' => fake()->postcode(),
             'latitude' => fake()->optional()->latitude(),
